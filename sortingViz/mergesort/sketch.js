9413909d1c12244a3857a2921944629517114c79
anthony-innamorato <ai974@nyu.edu>
2020-08-06 17:49:52 -0400

    print sorted arr

--- sortingViz/mergesort/sketch.js
+++ sortingViz/mergesort/sketch.js
@@ -1,7 +1,22 @@
+var ARR = [];
+var WIDTH = 1200; var HEIGHT = 700;
+
 function setup() {
-  createCanvas(1200, 700);
+  createCanvas(WIDTH, HEIGHT);
+  //make array based on dimensions
+  for (let i = 0; i < min(WIDTH, HEIGHT); i++) {
+    ARR[i] = i+1;
+  }
+  stroke(0, 255, 0);
 }
 
 function draw() {
-  background(100);
+  background(0);
+  for (let i = 0; i < ARR.length; i++) {
+    line(i, HEIGHT, i, HEIGHT-ARR[i]);
+  }
+  //enum show clean array
+  //enum show shuffling
+  //enum show sorting
+  //enum show sorted
 }
